b7bd660cc1b7abe48075286b44c7e7b5530855c3
Romain Judic <romain.judic@talkspirit.com>
2022-03-30 19:31:28 +0200

    Create ContentType store

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,6 +1,7 @@
 import { PlainClientAPI } from 'contentful-management';
 import { Paragraph } from '@contentful/f36-components';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
+import { ContentTypesProvider } from '../stores/ContentTypeStore';
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
@@ -11,7 +12,11 @@ const Field = (props: FieldProps) => {
   // If you only want to extend Contentful's default editing experience
   // reuse Contentful's editor components
   // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
+  return (
+    <ContentTypesProvider cma={props.cma}>
+      <Paragraph>Hello Entry Field Component</Paragraph>
+    </ContentTypesProvider>
+  );
 };
 
 export default Field;
---
+++ src/stores/ContentTypeStore.ts
@@ -0,0 +1,63 @@
+import { useCallback, useReducer } from 'react';
+import { ContentTypeProps, PlainClientAPI } from 'contentful-management';
+import constate from 'constate';
+
+interface ContentTypeMap {
+  [id: string]: 'failed' | undefined | ContentTypeProps;
+}
+
+type Action =
+  | { type: 'set_content_type', id: string, contentType: ContentTypeProps }
+  | { type: 'set_content_type_failed', id: string };
+
+
+function reducer(state: ContentTypeMap, action: Action): ContentTypeMap {
+  switch (action.type) {
+    case 'set_content_type':
+      return {
+        ...state,
+        [action.id]: action.contentType,
+      };
+    case 'set_content_type_failed':
+      return {
+        ...state,
+        [action.id]: 'failed',
+      };
+    default:
+      return state;
+  }
+}
+
+const initialState = {};
+
+
+function useContentTypeStore(props: { cma: PlainClientAPI }) {
+  const { cma } = props;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const loadContentType = useCallback(async (id: string) => {
+    try {
+      const contentType = await cma.contentType.get({ contentTypeId: id });
+      dispatch({ type: 'set_content_type', id, contentType });
+      return contentType;
+    } catch (error) {
+      dispatch({ type: 'set_content_type_failed', id });
+    }
+  }, [cma]);
+
+  const getOrLoadContentType = useCallback(async (id: string) => {
+    if (id in state && state[id] !== 'failed') {
+      return state[id] as ContentTypeProps;
+    }
+
+    return loadContentType(id);
+  }, [loadContentType, state]);
+
+  return { getOrLoadContentType };
+}
+
+
+const [ContentTypesProvider, useContentTypes] = constate(useContentTypeStore);
+
+
+export { ContentTypesProvider, useContentTypes };
